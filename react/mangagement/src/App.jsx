d90d12e68b21e2fb06b600a41a98aa56cb4b80c5
Ricardo <3239468786lzy@gmail.com>
2025-07-05 20:38:12 +0800

    react-management2

--- react/mangagement/src/App.jsx
+++ react/mangagement/src/App.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './views/login/Login';
 import Layout from './views/layout/index';
 import Public from './views/public/index';
 import Home from './views/home/index';
 import Score from './views/score/index';
 
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to='/login' replace />} />
           <Route path='/login' element={<Login />} />
           <Route path='/layout' element={<Layout />}>
             <Route path='' element={<Home />}></Route>
