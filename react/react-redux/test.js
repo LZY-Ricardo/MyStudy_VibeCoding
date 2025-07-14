5b2075e51279b4461b824cbbb93befb918e089b5
Ricardo <3239468786lzy@gmail.com>
2025-07-14 11:07:07 +0800

    react-redux update

--- react/react-redux/test.js
+++ react/react-redux/test.js
@@ -3,3 +3,9 @@ const arr = [1, 2, 3]
 arr.map((item, index, arr) => {
     console.log(item, index, arr);
 })
+
+const newArr = arr.filter((item, index ,arr) => {
+    return (
+        item >= 2
+    )
+})
