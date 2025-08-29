705992a9710489a28a23b6b6ecdca42c119e0fb4
Ricardo <3239468786lzy@gmail.com>
2025-08-29 12:22:06 +0800

    playground

--- 面试题/react/react-playground/src/components/Preview/index.tsx
+++ 面试题/react/react-playground/src/components/Preview/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { compile } from './compiler'
+import { useContext } from 'react'
+import { PlaygroundContext } from '../../ReactPlayground/PlaygroundContext'
+import  Editor  from '../CodeEditor/Editor/index'
 
 export default function Preview() {
+  const { files } = useContext(PlaygroundContext)
+  const code = compile(files) as string
+  
+  
   return (
-    <div>Preview</div>
+    <div style={{height: '100%'}}>
+      <Editor
+        file={{
+          name: 'Preview',
+          value: code,
+          language: 'javascript',
+        }}
+      />
+    </div>
   )
 }
