3f5c1da7ac45685f080351fb4b50b6b243d3d52e
Ricardo <3239468786lzy@gmail.com>
2025-09-05 21:43:01 +0800

    解析url

---
+++ 面试题/大厂经典考题/解析 url/index.js
@@ -0,0 +1,35 @@
+const url = 'https://www.baidu.com/order/home?user=Tom&id=123&city=%E5%8D%97%E6%98%8C&id=6666' // 'https://www.baidu.com/order/home?user=Tom&id=123&city=南昌'
+const urlDecode = decodeURI(url)
+
+const output = {
+    protocol: 'https:',
+    hostName: 'www.baidu.com',
+    path: '/order',
+    query: {
+        user: 'Tom',
+        id: [123, 6666],
+        city: '南昌'
+    }
+}
+
+function parser(url) {
+    const protocolArr = url.split('://')
+    const protocol = protocolArr[0]
+    const hostName = protocolArr[1].split('/')[0]
+    const path = protocolArr[1].split('?')[0].slice(hostName.length)
+    const queryArr = protocolArr[1].split('?')[1].split('&')
+    const query = {}
+    queryArr.forEach((item) => {
+        const itemArr = item.split('=')
+        const key = itemArr[0]
+        const value = itemArr[1]
+        query[key] = query[key] ? [...query[key], value] : [value]
+    })
+    return {
+        protocol,
+        hostName,
+        path,
+        query
+    }
+}
+console.log(parser(urlDecode));
