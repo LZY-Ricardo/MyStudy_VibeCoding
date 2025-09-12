d68287bd66346ad7c3a813fee5a8fb6f5ce9a744
Ricardo <3239468786lzy@gmail.com>
2025-09-12 22:41:20 +0800

    视言

---
+++ 面试题/大厂经典考题/视言/2.js
@@ -0,0 +1,29 @@
+setTimeout(() => {
+    console.log(1);
+}, 20);
+
+console.log(2);
+
+setTimeout(() => {
+    console.log(3);
+}, 10);
+
+console.log(4);
+
+for (let i = 0; i < 90000000; i++) {
+    // do soming 80ms左右
+}
+
+console.log(5);
+
+setTimeout(() => {
+    console.log(6);
+}, 8);
+
+console.log(7);
+
+setTimeout(() => {
+    console.log(8);
+}, 15);
+
+console.log(9);
