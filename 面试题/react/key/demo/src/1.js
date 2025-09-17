9ae4fc710e92bdbb670b3730e0cb0777be83c539
Ricardo <3239468786lzy@gmail.com>
2025-09-17 16:42:19 +0800

    key

---
+++ 面试题/react/key/demo/src/1.js
@@ -0,0 +1,9 @@
+vodm = {
+    tag: 'ul',
+    children: [
+        {tag: 'li', children: '张三', key: 1},
+        {tag: 'li', children: '李四', key: 2},
+        {tag: 'li', children: '王五', key: 3},
+    ],
+    key: 1,
+}
