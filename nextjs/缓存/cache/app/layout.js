e40090579aa46d1aae468970ec255aa53ed34b97
Ricardo <3239468786lzy@gmail.com>
2025-10-17 15:06:31 +0800

    cache

---
+++ nextjs/缓存/cache/app/layout.js
@@ -0,0 +1,17 @@
+// app/layout.js
+import Link from "next/link";
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                <div>
+                    <Link href="/a">Link to /a</Link>
+                    <br />
+                    <Link href="/b">Link to /b</Link>
+                </div>
+                {children}
+            </body>
+        </html>
+    )
+}
