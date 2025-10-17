08c591eb5bbd77fa0288bf37812d211123aae8d9
Ricardo <3239468786lzy@gmail.com>
2025-10-17 15:31:18 +0800

    update

--- nextjs/数据获取/cache/app/api/revalidateTag/route.js
+++ nextjs/数据获取/cache/app/api/revalidateTag/route.js
@@ -1,10 +1,7 @@
 import { revalidateTag } from "next/cache";
-import { redirect } from "next/navigation";
 
 export async function GET(request) {
     const tag = request.nextUrl.searchParams.get('tag')
     revalidateTag(tag)
-
-    // 重定向回首页，这样用户就能看到新的图片
-    redirect('/')
+    return Response.json({ revalidated: true, now: Date.now() })
 }
