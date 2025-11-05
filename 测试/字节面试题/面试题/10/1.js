7b61f4267efd87f0b6439882cc8fd770dd749335
Ricardo <3239468786lzy@gmail.com>
2025-11-05 22:02:48 +0800

    交叉并集

--- 测试/字节面试题/面试题/10/1.js
+++ 测试/字节面试题/面试题/10/1.js
@@ -5,3 +5,22 @@
 // 差集（A 有 B 无）
 const arr1 = [1, 2, 3];
 const arr2 = [2, 3, 4];
+
+// 交集
+function intersection(arr1, arr2) { 
+    return arr1.filter(item => arr2.includes(item))
+}
+
+// 并集
+function union(arr1, arr2) {
+    return [...new Set([...arr1, ...arr2])]
+}
+
+// 差集
+function difference(arr1, arr2) {
+    return arr1.filter(item => !arr2.includes(item))
+}
+
+console.log(intersection(arr1, arr2))
+console.log(union(arr1, arr2))
+console.log(difference(arr1, arr2))
