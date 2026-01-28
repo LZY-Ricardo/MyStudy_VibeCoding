92a2390eb73e7920f2eabe8b0e503545de26b862
Ricardo <3239468786lzy@gmail.com>
2026-01-28 16:23:26 +0800

    feat(备份): 新增 watch 监听属性示例

---
+++ Vue/hello_vue3/备份/013_watch监视情况一/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- html -->
+    <Person />  <!-- 使用 Person 组件 -->
+</template>
+
+<script lang="ts">
+// JS 或 TS
+import Person from './components/Person.vue'
+export default {
+    name: 'App', // 组件名称
+    components: { // 注册组件
+        Person,
+    },
+}
+</script>
+
+<style>
+/* CSS */
+.app {
+    text-align: center;
+    margin-top: 60px;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.1);
+}
+</style>
