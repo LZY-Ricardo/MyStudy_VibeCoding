1c55e6e56206d176fd5867f7ed354ec510f812da
Ricardo <3239468786lzy@gmail.com>
2026-02-09 17:15:47 +0800

    feat(vue): 新增 Vue2 测试项目

---
+++ Vue/vue2_test/src/components/Person.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="person">
+    <h2>
+      当前求和位: {{ sum  }}
+    </h2>
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+ /* eslint-disable */
+export default {
+  name: 'Person',
+  // 数据
+  data() {
+    return {
+      sum: 1
+    }
+  },
+  // 方法
+  methods: {
+    add() {
+      this.sum += 1
+    }
+  },
+  // 创建前的钩子
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  // 创建后的钩子
+  created() {
+    console.log('created');
+  },
+  // 挂载前的钩子
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  // 挂载后的钩子
+  mounted() {
+    console.log('mounted');
+  },
+  // 更新前的钩子
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  // 更新后的钩子
+  updated() {
+    console.log('updated');
+  },
+  // 销毁前的钩子
+  beforeDestroy() {
+    console.log('beforeDestroy');
+  },
+  // 销毁后的钩子
+  destroyed() {
+    console.log('destroyed');
+  }
+}
+</script>
+
+<style scoped>
+  .person{
+    border: 1px solid #ccc;
+    width: 200px;
+    line-height: 100px;
+    margin: 20px auto;
+    box-shadow: darkblue 1px 1px 5px;
+  }
+</style>
