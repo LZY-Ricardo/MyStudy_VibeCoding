82d974ae11725778632bf25caa0a8e8382159183
Ricardo <3239468786lzy@gmail.com>
2026-02-13 15:46:39 +0800

    refactor(hello_vue3): 移除路由系统，新增组件示例
    删除基于路由的多页面架构，改为基础组件演示项目。新增 Count 计数器与 LoveTalk 情话组件，移除 Header 及所有页面组件，简化项目结构。

---
+++ Vue/hello_vue3/备份/033_pinia准备一个效果/components/Count.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="count"> 
+    <h2>当前求和为:{{ sum }}</h2>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="add">加</button>
+    <button @click="minus">减</button>
+  </div>
+</template>
+
+<script setup lang="ts" name="Count">
+import { ref } from 'vue';
+
+// 数据
+let sum = ref(0); // 当前求和的结果
+let n = ref(1); // 用户选择的数字
+
+// 方法
+const add = () => {
+  sum.value += n.value;
+}
+const minus = () => {
+  sum.value -= n.value;
+}
+</script>
+
+<style scoped>
+  .count{
+    border: 1px solid #ccc;
+    width: 400px;
+    margin: 20px auto;
+    text-align: center;
+    line-height: 200px;
+    background-color: #eee;
+    box-shadow: 2px 2px 5px #ccc;
+  }
+
+  select, button{
+    margin: auto 10px;
+    padding: 5px 10px;
+    font-size: 16px;
+  }
+</style>
---
+++ Vue/hello_vue3/备份/033_pinia准备一个效果/components/LoveTalk.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="talk">
+    <button @click="getLoveTalk">获取一句土味情话</button>
+    <ul>
+      <li v-for="value in talkList" :key="value.id">{{ value.content }}</li>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts" name="LoveTalk">
+import { reactive } from 'vue';
+import axios from 'axios';
+import { nanoid } from 'nanoid';
+
+interface Talk {
+  id: string;
+  content: string;
+}
+
+let talkList = reactive < Talk[] > ([
+  { id: '1',
+    content: '你是我的小呀小苹果，怎么爱你都不嫌多'
+  },
+  {
+    id: '2',
+    content: '我想和你一起吹吹风，看看海，吃吃烧烤'
+  },
+  {
+    id: '3',
+    content: '我想和你一起看日出，吃吃早点，逛逛街'
+  }
+]);
+
+// 方法
+async function getLoveTalk() {
+  // 使用一言API获取情话
+  let url = 'https://v1.hitokoto.cn/?c=i&encode=json';
+  // 发送请求
+  const res = await axios.get(url);
+  console.log(res);
+  // 连续结构赋值 + 重命名
+  let {data: {hitokoto: content}} = res;
+  // 把请求回来的字符串，包装成一个对象
+  let obj: Talk = {
+    id: nanoid(),
+    content
+  }
+  console.log(obj);
+
+  talkList.unshift(obj);
+}
+
+</script>
+<style scoped>
+  .talk{
+    border: 1px solid #ccc;
+    width: 400px;
+    margin: 20px auto;
+    text-align: center;
+    line-height: 20px;
+    background-color: #eee;
+    box-shadow: 2px 2px 5px #ccc;
+  }
+</style>
