cc22a743bca1df24527d89f73f99c8da3d5cf169
Ricardo <3239468786lzy@gmail.com>
2026-02-25 14:22:32 +0800

    feat: 添加Suspense功能并重构App组件
    重构App组件以使用Suspense功能，添加新的Child组件用于异步数据展示
    删除不再使用的Modal组件和图片资源

---
+++ Vue/hello_vue3/src/Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="child">
+    <h2>我是Child组件</h2>
+    <p>当前求和：{{ sum }}</p>
+  </div>
+</template>
+
+<script setup lang="ts" name="Child">
+import { ref } from 'vue'
+import axios from 'axios';
+
+const sum = ref(0)
+let { data: { hitokoto } } = await axios.get('https://v1.hitokoto.cn/?c=l')
+console.log(hitokoto);
+
+
+
+</script>
+
+<style scoped>
+.child {
+  padding: 10px;
+  background-color: skyblue;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+</style>
